da5b9a0647f44b78723373b1551f1f1effbe28db
Cem <cemekinci1917@gmail.com>
2026-05-25 13:04:09 +0300

    fix: Create minimal DbContext with User entity
    Removed unused DbSet properties and related usings.

--- BaskentEnerji.Data/Contexts/BaskentEnerjiDbContext.cs
+++ BaskentEnerji.Data/Contexts/BaskentEnerjiDbContext.cs
@@ -1,12 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using BaskentEnerji.Entity.Entities.User;
-using BaskentEnerji.Entity.Entities.Blog;
-using BaskentEnerji.Entity.Entities.Coin;
-using BaskentEnerji.Entity.Entities.ExchangeOffice.Currency;
-using BaskentEnerji.Entity.Entities.ExchangeOffice.Expense;
-using BaskentEnerji.Entity.Entities.ExchangeOffice.Office;
-using BaskentEnerji.Entity.Entities.ExchangeOffice.Party;
-using BaskentEnerji.Entity.Entities.Site;
 using System;
 
 namespace BaskentEnerji.Data.Contexts
@@ -23,22 +16,7 @@ namespace BaskentEnerji.Data.Contexts
                                                                         modelBuilder.HasDefaultSchema("mtturkey_exchange");
                                                                                 }
 
-                                                                                        // User
+                                                                                        // User - Temel Entity
                                                                                                 public DbSet<User> Users { get; set; }
-
-                                                                                                        // Coin
-                                                                                                                public DbSet<Coin> Coins { get; set; }
-                                                                                                                
-                                                                                                                        // ExchangeOffice
-                                                                                                                                public DbSet<Currency> Currencies { get; set; }
-                                                                                                                                        public DbSet<ExchangeRate> ExchangeRates { get; set; }
-                                                                                                                                                public DbSet<ExchangeRateHistory> ExchangeRateHistories { get; set; }
-                                                                                                                                                        public DbSet<ExchangeSettings> ExchangeSettings { get; set; }
-                                                                                                                                                                public DbSet<ExternalDataSource> ExternalDataSources { get; set; }
-                                                                                                                                                                        public DbSet<ExternalRateCache> ExternalRateCaches { get; set; }
-                                                                                                                                                                                public DbSet<PendingRateApproval> PendingRateApprovals { get; set; }
-                                                                                                                                                                                        public DbSet<Expense> Expenses { get; set; }
-                                                                                                                                                                                                public DbSet<Office> Offices { get; set; }
-                                                                                                                                                                                                        public DbSet<Party> Parties { get; set; }
-                                                                                                                                                                                                            }
-                                                                                                                                                                                                            }
+                                                                                                    }
+                                                                                                    }
