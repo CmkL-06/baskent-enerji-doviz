e8972401737ab786a748b1a6c90037bd520637ec
Cem <cemekinci1917@gmail.com>
2026-05-25 13:09:15 +0300

    Add DbSet properties for various entities

--- BaskentEnerji.Data/Contexts/BaskentEnerjiDbContext.cs
+++ BaskentEnerji.Data/Contexts/BaskentEnerjiDbContext.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using BaskentEnerji.Entity.Entities.User;
+using BaskentEnerji.Entity.Entities.Blog;
+using BaskentEnerji.Entity.Entities.Coin;
+using BaskentEnerji.Entity.Entities.ExchangeOffice;
+using BaskentEnerji.Entity.Entities.Site;
 using System;
 
 namespace BaskentEnerji.Data.Contexts
@@ -17,6 +21,35 @@ namespace BaskentEnerji.Data.Contexts
                                                                                 }
 
                                                                                         // User - Temel Entity
-                                                                                                public DbSet<User> Users { get; set; }
+                                                                                                public DbSet<User> Users { get; set; 
+                                                                                                
+                                                                                                        // Blog Entities
+                                                                                                                public DbSet<Blog_Article> Blog_Articles { get; set; }
+                                                                                                                        public DbSet<Blog_Article_Category> Blog_Article_Categories { get; set; }
+                                                                                                                                public DbSet<Blog_Article_Comment> Blog_Article_Comments { get; set; }
+                                                                                                                                        public DbSet<Blog_Article_Tag> Blog_Article_Tags { get; set; }
+                                                                                                                                                public DbSet<Blog_Article_Visit> Blog_Article_Visits { get; set; }
+                                                                                                                                                        public DbSet<Blog_Category> Blog_Categories { get; set; }
+                                                                                                                                                                public DbSet<Blog_Category_Tag> Blog_Category_Tags { get; set; }
+                                                                                                                                                                        
+                                                                                                                                                                                // Coin Entities
+                                                                                                                                                                                        public DbSet<Coin> Coins { get; set; }
+                                                                                                                                                                                                public DbSet<Coin_User_Favorite> Coin_User_Favorites { get; set; }
+                                                                                                                                                                                                        public DbSet<Coin_User_Table> Coin_User_Tables { get; set; }
+                                                                                                                                                                                                                
+                                                                                                                                                                                                                        // ExchangeOffice Entities
+                                                                                                                                                                                                                                public DbSet<ExchangeGate> ExchangeGates { get; set; }
+                                                                                                                                                                                                                                        public DbSet<ExchangeOffice> ExchangeOffices { get; set; }
+                                                                                                                                                                                                                                                public DbSet<Currency> Currencies { get; set; }
+                                                                                                                                                                                                                                                        public DbSet<Expense> Expenses { get; set; }
+                                                                                                                                                                                                                                                                public DbSet<Office> Offices { get; set; }
+                                                                                                                                                                                                                                                                        public DbSet<Party> Parties { get; set; }
+                                                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                                        // Site Entities
+                                                                                                                                                                                                                                                                                                public DbSet<Slider> Sliders { get; set; }
+                                                                                                                                                                                                                                                                                                        public DbSet<Slider_Item> Slider_Items { get; set; }
+                                                                                                                                                                                                                                                                                                                public DbSet<Tag> Tags { get; set; }
+                                                                                                                                                                                                                                                                                                                        public DbSet<Theme> Themes { get; set; }
+                                                                                                                                                                                                                                                                                                                                }
                                                                                                     }
                                                                                                     }
